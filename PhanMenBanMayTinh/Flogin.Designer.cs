02e7cb7f90c7e00d67a54695126d038ac7bb5b41
hieu92264 <104044219+hieu92264@users.noreply.github.com>
2023-04-04 01:05:30 +0700

    Add project files.

---
+++ PhanMenBanMayTinh/Flogin.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace PhanMenBanMayTinh
+{
+    partial class Flogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BTN_LoginExit = new System.Windows.Forms.Button();
+            this.BTN_Login = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.text_LoginPass = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.text_LoginName = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.BTN_LoginExit);
+            this.panel1.Controls.Add(this.BTN_Login);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(13, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(328, 156);
+            this.panel1.TabIndex = 0;
+            // 
+            // BTN_LoginExit
+            // 
+            this.BTN_LoginExit.Location = new System.Drawing.Point(237, 109);
+            this.BTN_LoginExit.Name = "BTN_LoginExit";
+            this.BTN_LoginExit.Size = new System.Drawing.Size(75, 32);
+            this.BTN_LoginExit.TabIndex = 4;
+            this.BTN_LoginExit.Text = "Thoát";
+            this.BTN_LoginExit.UseVisualStyleBackColor = true;
+            this.BTN_LoginExit.Click += new System.EventHandler(this.BTN_LoginExit_Click);
+            // 
+            // BTN_Login
+            // 
+            this.BTN_Login.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_Login.Location = new System.Drawing.Point(142, 109);
+            this.BTN_Login.Name = "BTN_Login";
+            this.BTN_Login.Size = new System.Drawing.Size(75, 32);
+            this.BTN_Login.TabIndex = 3;
+            this.BTN_Login.Text = "Đăng nhập";
+            this.BTN_Login.UseVisualStyleBackColor = true;
+            this.BTN_Login.Click += new System.EventHandler(this.BTN_Login_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.text_LoginPass);
+            this.panel3.Location = new System.Drawing.Point(3, 54);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(322, 49);
+            this.panel3.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mật khẩu:";
+            // 
+            // text_LoginPass
+            // 
+            this.text_LoginPass.Location = new System.Drawing.Point(139, 11);
+            this.text_LoginPass.Name = "text_LoginPass";
+            this.text_LoginPass.Size = new System.Drawing.Size(170, 20);
+            this.text_LoginPass.TabIndex = 2;
+            this.text_LoginPass.UseSystemPasswordChar = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.text_LoginName);
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(322, 49);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tên đăng nhập:";
+            // 
+            // text_LoginName
+            // 
+            this.text_LoginName.Location = new System.Drawing.Point(139, 9);
+            this.text_LoginName.Name = "text_LoginName";
+            this.text_LoginName.Size = new System.Drawing.Size(170, 20);
+            this.text_LoginName.TabIndex = 1;
+            // 
+            // Flogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 179);
+            this.Controls.Add(this.panel1);
+            this.Name = "Flogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng nhập";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Flogin_FormClosing);
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox text_LoginPass;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox text_LoginName;
+        private System.Windows.Forms.Button BTN_LoginExit;
+        private System.Windows.Forms.Button BTN_Login;
+    }
+}
---
+++ PhanMenBanMayTinh/Flogin.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhanMenBanMayTinh
+{
+    public partial class Flogin : Form
+    {
+        public Flogin()
+        {
+            InitializeComponent();
+            AcceptButton = BTN_Login;
+            CancelButton = BTN_LoginExit;
+        }
+
+        private void Flogin_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult kq;
+            kq = MessageBox.Show("Bạn có thực sự muốn thoát?", "Thông báo!", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (kq != DialogResult.OK)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void BTN_LoginExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void BTN_Login_Click(object sender, EventArgs e)
+        {
+            int username = Convert.ToInt32(text_LoginName.Text);
+            string password = text_LoginPass.Text;
+            try
+            {
+                string query = "select *from Accounts where id = '" + username + "'and PASS ='" + password + "'";
+                string stringconnection = "Data Source=ADMIN-PC;Initial Catalog=QLMT2;Integrated Security=True";
+                using (SqlConnection connection = new SqlConnection(stringconnection))
+                {
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand(query, connection);
+                    SqlDataReader data = cmd.ExecuteReader();
+                    if (data.Read())
+                    {
+                        this.Hide();
+                        QLBMT frm = new QLBMT();
+                        frm.ShowDialog();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Tài khoản hoặc mật khẩu không đúng!");
+                    }
+                    connection.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi kết nối");
+            }
+        }
+    }
+            
+}
