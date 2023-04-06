69c5927c62509c2e56905d9258bc922f95adfb58
hieu92264 <104044219+hieu92264@users.noreply.github.com>
2023-04-06 18:57:00 +0700

    test

--- PhanMenBanMayTinh/DataProvide.cs
+++ PhanMenBanMayTinh/DataProvide.cs
@@ -11,7 +11,7 @@ namespace PhanMenBanMayTinh
 {
     public class DataProvide
     {
-        //cmt
+        
         private string stringconnection = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename="+Application.StartupPath+@"\QLMT.mdf;Integrated Security=True;Connect Timeout=30";
         public DataTable ExcuteQuery(string query) // tra ve mot bang database
         {
